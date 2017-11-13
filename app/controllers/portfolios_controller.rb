93686ddd95703461cf714d5b0e9191f8e4baa6c4
Alex Chang <alexchangbz@Alexs-MacBook-Pro.local>
2017-11-13 15:02:47 +0800

    Implemeted destroy function for portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -44,4 +44,14 @@ class PortfoliosController < ApplicationController
   		@portfolio_item = Portfolio.find(params[:id])
   	end
 
+  	def destroy
+  		@portfolio_item = Portfolio.find(params[:id])
+
+    	@portfolio_item.destroy
+    	respond_to do |format|
+      		format.html { redirect_to portfolios_url, notice: 'Portfolio item was removed.' }
+
+    	end
+  	end
+
 end
